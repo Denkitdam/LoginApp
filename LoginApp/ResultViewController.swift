775512cc94cbdd12895538058b585e826beedbe9
denkitdam <cryptoden145@gmail.com>
2023-03-31 10:14:24 +0300

    added LoginViewControoller and ResultViewController

---
+++ LoginApp/ResultViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ResultViewController.swift
+//  LoginApp
+//
+//  Created by Denis Kitaev on 31.03.2023.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}

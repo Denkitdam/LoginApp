c5d89991d708abe9692bee0eb4088ca52d2b7650
denkitdam <cryptoden145@gmail.com>
2023-04-05 22:04:44 +0300

    Outlets connected for InfoViewController

--- LoginApp/ViewControllers /InfoViewController.swift
+++ LoginApp/ViewControllers /InfoViewController.swift
@@ -7,23 +7,13 @@
 
 import UIKit
 
-class InfoViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+final class InfoViewController: UIViewController {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    //@IBOutlet var image: UIImage!
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var dateOfBirthLabel: UILabel!
+    @IBOutlet var placeOfBirthLabel: UILabel!
+    @IBOutlet var codingExperienceLabel: UILabel!
 
 }

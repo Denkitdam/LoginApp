eba4247b8428b6ef57a289cb36207f30c492fb79
denkitdam <cryptoden145@gmail.com>
2023-04-05 22:05:21 +0300

    Tab bar created

---
+++ LoginApp/ViewControllers /TabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarControllerViewController.swift
+//  LoginApp
+//
+//  Created by Denis Kitaev on 05.04.2023.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
